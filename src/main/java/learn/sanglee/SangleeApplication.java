acbc36d9fe17c00ea59ead755f0526b29204bff0
SangNyungLee <lsy6747@gmail.com>
2024-09-02 22:11:38 +0900

    Initial commit

---
+++ src/main/java/learn/sanglee/SangleeApplication.java
@@ -0,0 +1,13 @@
+package learn.sanglee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/sanglee/SangleeApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.sanglee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
